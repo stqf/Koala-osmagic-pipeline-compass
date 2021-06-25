7d6f9c6b9da2097fab5fd586ed1949a602b41943
yanying <yanying@yourangroup.com>
2021-06-25 08:55:55 +0800

    feat:添加工具脚本down.groovy

---
+++ vars/down.groovy
@@ -0,0 +1,15 @@
+def clone(String wsItem, String branch, String url) {
+    def saItem = env.get("Sauce_Access")
+    def branchItem = branch == null ? "master" : branch
+    def reallyUrl = url.replaceAll("http://", "http://$saItem@")
+    sh """
+        if [ -d "$wsItem" ]; then
+            cd $wsItem && git reset --hard HEAD && git pull 
+            echo "[INFO ] 更新代码完成 ..."
+        else
+            git clone $reallyUrl -b $branchItem $wsItem
+            echo "[INFO ] 拉取代码完成 ..."
+        fi
+    """
+    println("[DEBUG] $wsItem($branch) clone finish ... ")
+}
