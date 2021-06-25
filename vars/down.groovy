6d3102260c150e19097c14c9dc6ef4f21f356ef0
yanying <yanying@yourangroup.com>
2021-06-25 10:42:07 +0800

    fix:down.groovy继续完善

--- vars/down.groovy
+++ vars/down.groovy
@@ -1,15 +1,24 @@
-def clone(String wsItem, String branch, String url) {
+/**
+ * GIT拉取代码
+ * @param wsItem 工作路径
+ * @param projectItem 项目
+ * @param branch 分支
+ * @param url 代码地址
+ * @return
+ */
+def clone(String wsItem, String projectItem, String branch, String url) {
     //def saItem = env.get("Sauce_Access")
     def branchItem = branch == null ? "master" : branch
     def reallyUrl = url.replaceAll("http://", "http://${env.Sauce_Access}@")
     sh """
-        if [ -d "$wsItem" ]; then
-            cd $wsItem && git reset --hard HEAD && git pull 
+        cd $wsItem
+        if [ -d "$projectItem" ]; then
+            cd $projectItem && git reset --hard HEAD && git pull 
             echo "[INFO ] 更新代码完成 ..."
         else
-            git clone $reallyUrl -b $branchItem $wsItem
+            git clone $reallyUrl -b $branchItem $projectItem
             echo "[INFO ] 拉取代码完成 ..."
         fi
     """
-    println("[DEBUG] $wsItem($branch) clone finish ... ")
+    println("[DEBUG] $projectItem($branch) clone finish ... ")
 }
