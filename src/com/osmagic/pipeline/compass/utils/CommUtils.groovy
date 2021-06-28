8cf042c9bef3875f2990a1465ff793df86f55c41
yanying <yanying@yourangroup.com>
2021-06-28 19:49:34 +0800

    feat:尝试依赖引入之三

--- src/com/osmagic/pipeline/compass/utils/CommUtils.groovy
+++ src/com/osmagic/pipeline/compass/utils/CommUtils.groovy
@@ -68,7 +68,7 @@ class CommUtils {
         }
         return XmlUtil.serialize(docItem)*/
 
-        double[] values = new double[]{0.33, 1.33, 0.27333, 0.3, 0.501, 0.444, 0.44, 0.34496, 0.33, 0.3, 0.292, 0.667};
+        double[] values = [0.33, 1.33, 0.27333, 0.3, 0.501, 0.444, 0.44, 0.34496, 0.33, 0.3, 0.292, 0.667]
         println("平均数：" + StatUtils.mean(values))
 
         return null
